6761f550cb36c9758524b114e487ac4db9ef3a5f
jyun27 <kimjyun27@gmail.com>
2025-05-06 02:02:29 +0900

    first commit

---
+++ src/main/java/com/example/Tomoto/TomotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tomoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tomoto/TomotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tomoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
